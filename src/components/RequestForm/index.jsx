e9f1beafa6d8c519dc0b27a0b5f2b3d3a5979937
Tyler Collins <tjscollins@gmail.com>
2018-02-12 11:17:33 -1000

    Initial RequestForm component

---
+++ src/components/RequestForm/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+class RequestForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <form />
+    );
+  }
+}
+
+RequestForm.propTypes = {
+
+};
+
+export default RequestForm;
