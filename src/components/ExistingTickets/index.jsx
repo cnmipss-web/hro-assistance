216c107420f71cf80bba1d30b21d4f37c33020e7
Tyler Collins <tjscollins@gmail.com>
2018-02-12 11:17:42 -1000

    Initial ExistingTickets component

---
+++ src/components/ExistingTickets/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+class ExistingTickets extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <table />
+    );
+  }
+}
+
+ExistingTickets.propTypes = {
+
+};
+
+export default ExistingTickets;
