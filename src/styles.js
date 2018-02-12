0604c50a5c550b5330d373ce5b30304dc7b93873
Tyler Collins <tjscollins@gmail.com>
2018-02-12 11:13:47 -1000

    CSS-in-JS theme file

---
+++ src/styles.js
@@ -0,0 +1,45 @@
+const breakPoints = {
+  sm: '576px',
+  md: '768px',
+  lg: '992px',
+  xlg: '1200px',
+};
+
+const colors = {
+  blue: '#1a4167',
+  darkBlue: '#022c55',
+  lightBlue: '#105194',
+  gold: '#b9972f',
+  lightGold: '#dfc253',
+  manila: '#f1d592',
+  white: 'white',
+
+};
+
+const fonts = {
+  baseFont: {
+    fontFamily: 'Arial, sans-serif',
+    fontSize: '16px',
+    fontWeight: '400',
+    textDecoration: 'none',
+    color: 'black',
+  },
+};
+
+const header = {
+  backgroundColor: colors.blue,
+  color: colors.white,
+};
+
+const a11y = {
+  focusOutline: `1px solid ${colors.gold} !important`,
+  altOutline: '1px solid red !important',
+};
+
+export default {
+  fonts,
+  header,
+  a11y,
+  breakPoints,
+};
+
