eb57cd83841f63f24442ce654bc4333b9ffcfc04
Tyler Collins <tjscollins@gmail.com>
2018-02-12 16:24:44 -1000

    Basic admin / non-admin distinction in component to display data on ExistingTickets

--- src/components/ExistingTickets/index.jsx
+++ src/components/ExistingTickets/index.jsx
@@ -1,22 +1,56 @@
-import React from 'react';
+import axios from 'axios';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import React from 'react';
+// import styled from 'styled-components';
 
 class ExistingTickets extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      admin: this.props.admin,
+    };
+  }
+
+  componentWillMount() {
+    const currentTickets = axios.get('dummy');
+    this.setState({
+      currentTickets,
+    });
+  }
+
+  ticketTable() {
+    const { currentTickets } = this.state;
+    return (
+      <table>
+        {currentTickets}
+      </table>
+    );
+  }
+
+  ticketEstimate() {
+    const { currentTickets } = this.state;
+    return (
+      <div>
+        {currentTickets}
+      </div>
+    );
   }
 
   render() {
     return (
-      <table />
+      <div>
+        {this.state.admin ? this.ticketTable() : this.ticketEstimate() }
+      </div>
     );
   }
 }
 
 ExistingTickets.propTypes = {
+  admin: PropTypes.bool,
+};
 
+ExistingTickets.defaultProps = {
+  admin: false,
 };
 
 export default ExistingTickets;
